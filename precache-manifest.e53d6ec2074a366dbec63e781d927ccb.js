6bf34dd119bf7a925c1ba13da28a76e1f9248ac2
meghaldonde <meghal.donde@gmail.com>
2020-08-17 18:10:12 -0400

    Updates

---
+++ precache-manifest.e53d6ec2074a366dbec63e781d927ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3f0f289b1a61e8c19d0f16919e6748",
+    "url": "/youtube-video-react.github.io/index.html"
+  },
+  {
+    "revision": "843b50fc40e5554295a6",
+    "url": "/youtube-video-react.github.io/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "9d13c413712b221177ba",
+    "url": "/youtube-video-react.github.io/static/js/2.c744b77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-video-react.github.io/static/js/2.c744b77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "843b50fc40e5554295a6",
+    "url": "/youtube-video-react.github.io/static/js/main.ef3a140f.chunk.js"
+  },
+  {
+    "revision": "bf3f1336aa84388c2044",
+    "url": "/youtube-video-react.github.io/static/js/runtime-main.3c000757.js"
+  }
+]);
